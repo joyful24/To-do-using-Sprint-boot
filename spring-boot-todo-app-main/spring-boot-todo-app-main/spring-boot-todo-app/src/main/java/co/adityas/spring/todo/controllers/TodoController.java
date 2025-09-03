e20482f213c3936b712bfc991017e4732cd37b73
ADITYA NARAYAN <129763286+joyful24@users.noreply.github.com>
2025-09-03 14:26:21 +0530

    Update TodoController.java

--- spring-boot-todo-app-main/spring-boot-todo-app-main/spring-boot-todo-app/src/main/java/co/adityas/spring/todo/controllers/TodoController.java
+++ spring-boot-todo-app-main/spring-boot-todo-app-main/spring-boot-todo-app/src/main/java/co/adityas/spring/todo/controllers/TodoController.java
@@ -1,4 +1,4 @@
-package co.sohamds.spring.todo.controllers;
+package co.adityas.spring.todo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import co.sohamds.spring.todo.domain.Todo;
-import co.sohamds.spring.todo.repository.TodoRepository;
+import co.adityas.spring.todo.domain.Todo;
+import co.adityas.spring.todo.repository.TodoRepository;
 
 @Controller
 public class TodoController {
@@ -57,4 +57,5 @@ public String update(@PathVariable long id, Model model) {
 	model.addAttribute("todos", todoRepository.findAll());
 	return "redirect:/todos";
 }
+
 }
